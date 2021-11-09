14b0ee02b561c10c9b225bc1ea16e452b64f7c8b
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-11-09 17:49:06 -0500

    add sort and pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,14 @@
 //MODELS
 const Product = require('../models/product')
 
+const operatorMap = {
+    '>': '$gt',
+    '>=': '$gte',
+    '=': '$eq',
+    '<': '$lt',
+    '<=': '$lte'
+}
+
 
 const getAllProductsStatic = async (req, res) => {
     const {search} = req.query
@@ -17,7 +25,49 @@ const getAllProductsStatic = async (req, res) => {
 
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find(req?.query)
+    const { sort, fields, numericFilters } = req.query
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+
+    const queryObject = {}
+    const skip = (page - 1) * limit
+
+    if(numericFilters) {
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regEx,(match) => `-${operatorMap[match]}-`)
+
+        // console.log(filters)
+
+        const options = ['price','rating']
+        filters = filters.split(',').forEach((item) => {
+            const [field,operator, value ] = item.split('-')
+
+            if(options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value)}
+            }
+        })
+    }
+
+    let result = Product.find(queryObject ? queryObject : req?.query)
+
+    if(sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+
+    if(fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+
+
+    //paginacion
+    result = result.skip(skip).limit(limit)
+
+    const products = await result
+
     res.status(200).json({products, nbHits: products.length})
 }
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const errorHandlerMiddleware = async (err, req, res, next) => {
-    return res.status(500).json({ msg: 'Something went wrong, please try again' })
+    return res.status(500).json({ msg: err.message || 'Something went wrong, please try again' })
   }
 
   module.exports = errorHandlerMiddleware
